dec90c705b7041be150051acfe5903e4794b12d2
SelmanDemiray <100167578+SelmanDemiray@users.noreply.github.com>
2023-01-14 12:38:47 -0500

    Update Moving_AD_Objects.ps1

--- Active_Directory_Scripts/Moving_AD_Objects.ps1
+++ Active_Directory_Scripts/Moving_AD_Objects.ps1
@@ -1,6 +1,6 @@
-To move AD objects:
+#To move AD objects:
 Move-ADObject -Identity "CN=London Admins,CN=Users,DC=Adatum,DC=com" -TargetPath "OU=London,DC=Adatum,DC=com"
 
-CN = common name 
-DC = domain component 
-OU = orginizational unit 
+#CN = common name 
+#DC = domain component 
+#OU = orginizational unit 
