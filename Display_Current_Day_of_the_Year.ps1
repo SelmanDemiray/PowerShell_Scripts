c1dbec170447295e9630fe558d40e25fc9173a7a
SelmanDemiray <100167578+SelmanDemiray@users.noreply.github.com>
2023-01-14 11:12:58 -0500

    Delete Display_Current_Day_of_the_Year.ps1

--- Display_Current_Day_of_the_Year.ps1
+++
@@ -1,5 +0,0 @@
-Get-Date | get-member 
-the "get-member" command is used to fetch which properties could be used with the "get-date" command 
-
-To get the date
-Get-Date | Select-Object –Property DayOfYear
